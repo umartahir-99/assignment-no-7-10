34ed0a700757dc278948877eeb90d028be68e5e1
umartahir-99 <tahirumar835@gmail.com>
2024-12-21 20:30:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Write a program that checks if a person is eligible to vote based on their age (age >= 18).
+
+
+
+var person = prompt("Enter age:");
+if(person >= 18){
+  console.log("Eligible for vote")
+
+}
+else{
+  console.log("Not Eligible")
+}
+
+
+
+
+
+
+
+
+
+
